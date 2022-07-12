c4aad5573864b9799f41e567576e4bd58e0475c0
shake <hiroki.yamako@ezweb.ne.jp>
2022-07-12 09:32:32 +0900

    [add]add interview request and response

--- api/routers/interview.py
+++ api/routers/interview.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import List
 
 from fastapi import APIRouter
@@ -7,9 +8,14 @@ import api.schemas.interview as interview_schema
 router = APIRouter()
 
 
-@router.post('/interview')
-async def create_interview():
-    pass
+@router.post('/interview', response_model=interview_schema.InterviewCreateResponse)
+async def create_interview(interview_body: interview_schema.InterviewCreate):
+    return interview_schema.InterviewCreateResponse(
+        id=1,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now(),
+        **interview_body.dict()
+    )
 
 
 @router.get('/interview/random', response_model=List[interview_schema.Interview])
@@ -22,9 +28,12 @@ async def obtain_bookmarked_interviews():
     return [interview_schema.Interview(id=1, content='これまで開発したもので一番自信があるものはなんですか')]
 
 
-@router.post('/interview/bookmark')
-async def add_bookmark_interview():
-    pass
+@router.post('/interview/bookmark', response_model=interview_schema.BookmarkInterviewCreateResponse)
+async def add_bookmark_interview(bookmark_body: interview_schema.BookmarkInterviewCreate):
+    return interview_schema.BookmarkInterviewCreateResponse(
+        user_id=1,
+        **bookmark_body.dict()
+    )
 
 
 @router.delete('/interview/bookmark')
