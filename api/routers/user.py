0d2c623565c9206ad2c7c380132170ed30b44b48
shake551 <73379887+shake551@users.noreply.github.com>
2022-07-17 04:55:25 +0900

    Revert "Feature/interview db"

--- api/routers/user.py
+++ api/routers/user.py
@@ -2,28 +2,24 @@ import datetime
 
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
 from starlette import status
 
 from api.config.user import ACCESS_TOKEN_EXPIRE_MINUTES
 import api.schemas.user as user_schema
-import api.repository.user as user_repository
-from api.db import get_db
 
 router = APIRouter()
 
 
 @router.post('/signup', response_model=user_schema.UserInDB)
-def signup(
-        form_data: OAuth2PasswordRequestForm = Depends(),
-        db: Session = Depends(get_db)
-):
-    return user_repository.signup(db=db, username=form_data.username, password=form_data.password)
+async def signup(form_data: OAuth2PasswordRequestForm = Depends()):
+    signup_request = user_schema.UserSignupRequest(username=form_data.username, plain_password=form_data.password)
+
+    return user_schema.signup(signup_request=signup_request)
 
 
 @router.post("/token", response_model=user_schema.Token)
-async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    user = user_repository.authenticate_user(db=db, username=form_data.username, password=form_data.password)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = user_schema.authenticate_user(user_schema.fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -31,12 +27,17 @@ async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = user_repository.create_access_token(
+    access_token = user_schema.create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.get("/users/me", response_model=user_schema.UserInDB)
-def read_users_me(current_user: user_schema.UserInDB = Depends(user_repository.get_current_active_user)):
+@router.get("/users/me/", response_model=user_schema.UserBase)
+async def read_users_me(current_user: user_schema.UserBase = Depends(user_schema.get_current_active_user)):
     return current_user
+
+
+@router.get("/users/me/items/")
+async def read_own_items(current_user: user_schema.UserBase = Depends(user_schema.get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,7 +1,14 @@
 import datetime
 from typing import Optional, Union
 
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from passlib.context import CryptContext
 from pydantic import BaseModel, Field
+from starlette import status
+
+from api.config.user import SECRET_KEY, ALGORITHM
 
 
 class Token(BaseModel):
@@ -19,19 +26,101 @@ class UserBase(BaseModel):
     hashed_password: Optional[str] = Field(None, example='hogehoge')
     disabled: bool = Field(False, example=False)
 
-    class Config:
-        orm_mode = True
-
 
 class UserInDB(UserBase):
     id: int
     created_at: datetime.datetime
     updated_at: datetime.datetime
 
-    class Config:
-        orm_mode = True
-
 
 class UserSignupRequest(BaseModel):
     username: Optional[str] = Field(None, example='test_user')
     plain_password: Optional[str] = Field(None, example='test_password')
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def signup(signup_request: UserSignupRequest):
+    return UserInDB(
+        id=1,
+        username=signup_request.username,
+        hashed_password=get_password_hash(signup_request.plain_password),
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now(),
+    )
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[datetime.timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+fake_users_db = {
+    "test_user": {
+        "id": 1,
+        "username": "test_user",
+        "hashed_password": "$2b$12$6KxDQ9zvm8.QFfm7OecP4eW7QJIIHxJXQWekJ1bmE0NXvfXONxJr6",
+        "disabled": False,
+        "created_at": datetime.datetime.now(),
+        "updated_at": datetime.datetime.now(),
+    }
+}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        print(token)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: UserBase = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
